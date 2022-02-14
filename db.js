be693697bc0e1e9550ff58d6f50a621b3896971b
Your Name <tejaszanzane2001@gmail.com>
2022-02-14 18:52:39 +0530

    lib-mng

---
+++ db.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: "mongodb+srv://LIb:Lib@lib-manage-sys.bbycw.mongodb.net/users",
+    // mongoURI: 'mongodb+srv://LIb:Lib@lib-manage-sys.bbycw.mongodb.net/Library?retryWrites=true',
+    secret:   'somethingsecrethere'
+}
+
+// const mongoURI = "mongodb+srv://LIb:Lib@lib-manage-sys.bbycw.mongodb.net/users";
